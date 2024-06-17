4b8849dfd766d852eeaef342690f0f99282684f1
Sarah Lucas <Sarah.Lucas3@nhs.net>
2024-06-17 09:58:04 +0100

    Setting up targets

---
+++ _targets.R
@@ -0,0 +1,27 @@
+# Load packages required to define the pipeline:
+library(targets)
+
+
+# Set target options:
+tar_option_set(
+  packages = c("dplyr") 
+)
+
+# Run the R scripts in the R/ folder with your custom functions:
+tar_source()
+
+# Target list:
+list(
+
+  tar_target(
+    icb_pop,
+    ICB_population_data_2022("ICB Population estimates 2022.csv")
+  ),
+  tar_target(
+    england_pop,
+    England_population_data("England population estimates 2018-2022.csv")
+  )
+
+  
+  
+)
