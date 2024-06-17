81a83723e585ba40be1faee0a9596f271b5918e2
Sarah Lucas <Sarah.Lucas3@nhs.net>
2024-06-17 10:06:51 +0100

    updating targets

--- _targets.R
+++ _targets.R
@@ -4,7 +4,7 @@ library(targets)
 
 # Set target options:
 tar_option_set(
-  packages = c("dplyr") 
+  packages = c("dplyr", "janitor") 
 )
 
 # Run the R scripts in the R/ folder with your custom functions:
@@ -15,11 +15,11 @@ list(
 
   tar_target(
     icb_pop,
-    ICB_population_data_2022("ICB Population estimates 2022.csv")
+    ICB_population_data_2022("Data/ICB Population estimates 2022.csv")
   ),
   tar_target(
     england_pop,
-    England_population_data("England population estimates 2018-2022.csv")
+    England_population_data("Data/England population estimates 2018-2022.csv")
   )
 
   
